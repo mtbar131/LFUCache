eb2a48ca66d10a1b7d3283795c4eb9906dac3954
Amit Barve <mtbar131@gmail.com>
2017-01-01 09:41:46 +0530

    added get/set steps to README

--- LFUCache.java
+++ LFUCache.java
@@ -66,30 +66,6 @@ public class LFUCache<K, V> {
 	return frequencyMap.get(frequency);
     }
 
-    // public void insert(K key, V value, int frequency) {
-    // 	/* Create freq Node with freq 1 if doesn't exist - Every new entry
-    // 	   has frequency 1 */
-    // 	FrequencyNode frequencyNode = null;
-    // 	System.out.println("Inserting new key " + key +  " for frequency " + frequency);
-    // 	if (!frequencyMap.containsKey(frequency)) {
-    // 	    frequencyNode = new FrequencyNode(frequency);
-    // 	    if (frequency == 1) {
-    // 		freqList.prepend(frequencyNode);
-    // 	    } else {
-    // 		freqList.insertAfter(frequencyMap.get(frequency - 1), frequencyNode);
-    // 	    }
-    // 	    frequencyMap.put(frequency, frequencyNode);
-    // 	} else {
-    // 	    frequencyNode = frequencyMap.get(frequency);
-    // 	}
-
-    // 	/* Create a new entry node - and insert into freqList of freq 1 */	
-    // 	LFUCacheEntry<K, V> entry = new LFUCacheEntry<K,V>(key, value, frequencyNode);
-    // 	frequencyNode.lfuCacheEntryList.append(entry);
-    // 	kvStore.put(key, entry);
-    // 	size++;
-    // }
-
     public void set(K key, V value) {
 
 	if (kvStore.containsKey(key)) {
@@ -134,22 +110,3 @@ public class LFUCache<K, V> {
 
 
 
-// Get procedure
-// 1. Get LFUCacheEntry reference from HashMap
-// 2. Find Frequency Node from the Entry Node
-// 3. Remove Entry Node from Frequency Node list and add it to next freuquency Node
-//    list
-//    a. If New frequency Node doesn't exist, create a new one and add its entry
-//       into frequency Node set
-//    b. If Previous Frequency Node list is now empty remove that frequency Node
-
-// Set procedure
-// 1. If same key already exists 
-//    Remove that LFUCacheEntry from Frequency Node & remove frequnecy Node if its
-//    Empty
-//    Remove that key & corresponding LFUCacheEntry from HashMap
-//    If Cache is full remove very first entry from very first frequency Node in list
-   
-// 2. Add New LFUCacheEntry into HashMap
-// 3. Add this new Entry into Frequency Node of frequency 1
-//    a. Create if such a node doesn't already exist
--- README.md
+++ README.md
@@ -9,3 +9,23 @@ get(key) - Get the value of the key if the key exists in the cache, otherwise re
 set(key, value) - Set or insert the value if the key is not already present. 
 When the cache reaches its capacity, it should invalidate the least frequently used item before inserting a new item. 
 when there is a tie (i.e., two or more keys that have the same frequency), the least recently used key would be evicted.
+
+// Get procedure
+// 1. Get LFUCacheEntry reference from HashMap
+// 2. Find Frequency Node from the Entry Node
+// 3. Remove Entry Node from Frequency Node list and add it to next freuquency Node
+//    list
+//    a. If New frequency Node doesn't exist, create a new one and add its entry
+//       into frequency Node set
+//    b. If Previous Frequency Node list is now empty remove that frequency Node
+
+// Set procedure
+// 1. If same key already exists 
+//    Remove that LFUCacheEntry from Frequency Node & remove frequnecy Node if its
+//    Empty
+//    Remove that key & corresponding LFUCacheEntry from HashMap
+//    If Cache is full remove very first entry from very first frequency Node in list
+   
+// 2. Add New LFUCacheEntry into HashMap
+// 3. Add this new Entry into Frequency Node of frequency 1
+//    a. Create if such a node doesn't already exist
